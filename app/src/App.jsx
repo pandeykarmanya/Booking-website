c17f4bdca5901f8e93e36564f34744735afc4814
Karmanya Pandey <karmanyapandey348@gmail.com>
2025-11-21 15:22:21 +0530

    feat: remove Hero component and associated imports from App

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Navbar />
-      <Hero />
     </div>
   );
 }
