6fb8f65d9056648e9078b8470bb100795c1e0206
Karmanya Pandey <karmanyapandey348@gmail.com>
2026-04-21 19:25:14 +0530

    feat: Add Footer component

---
+++ app/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="text-center bg-[#7a1c2e] py-4 text-xs text-gray-400">
+      © I.T.S 2026   •   Mohan Nagar, Ghaziabad  •  
+    </footer>
+  );
+}
+
+export default Footer;
