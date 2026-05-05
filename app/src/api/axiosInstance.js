382b4bb526cacf7c3a38533c672b814d66d3fc4d
Karmanya Pandey <karmanyapandey348@gmail.com>
2026-05-05 20:53:43 +0530

    fix: update axios instance baseURL to use correct API_BASE_URL

--- app/src/api/axiosInstance.js
+++ app/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { API_BASE_URL } from "../config";
 
 const instance = axios.create({
-  baseURL: `${import.meta.env.API_BASE_URL_URL}/api/v1`,
+  baseURL: API_BASE_URL,
   withCredentials: true,
 });
 
