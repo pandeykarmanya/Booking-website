ffb5e24a87005056458d1b97595f6188211a6e42
Karmanya Pandey <karmanyapandey348@gmail.com>
2026-05-05 20:29:13 +0530

    fix: update axios instance baseURL to use environment variable for API URL

--- app/src/api/axiosInstance.js
+++ app/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { API_BASE_URL } from "../config";
 
 const instance = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: `${import.meta.env.API_BASE_URL_URL}/api/v1`,
   withCredentials: true,
 });
 
